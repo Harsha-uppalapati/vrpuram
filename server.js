1697004505f822e688f57ef686e48bff20e8c3a5
Harshavardhan_uppalapati <harshau91@gmail.com>
2022-05-19 16:43:27 +0530

    db

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //db connection start
 let dbconnection = async () => {
-  await mongoose.connect("mongodb://localhost:27017/vrpuram");
-  // await mongoose.connect(env.parsed.dburl)
+  // await mongoose.connect("mongodb://localhost:27017/vrpuram");
+  await mongoose.connect(env.parsed.dburl)
   console.log("database is connected");
 };
 dbconnection();
